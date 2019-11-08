c54e21cf3f3faac16037c1b461fb7b75e336eefc
Akshay Choudhary <ak92choudhary@gmail.com>
2019-11-07 19:42:19 -0500

    worked on section 3

---
+++ src/main/java/com/company/SectionThree.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class SectionThree {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+
+        // challenge how would you go about changing the hello world to print your name?
+
+        System.out.println("Hello, Akshay");
+
+        // variables
+        int myFirstNumber = (10 + 5) + (2 * 10);
+
+        //challenge print out the value of myfirstnumber
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+
+        //final variable challenge
+        // create a new variable called myLastOne
+        // we want the value to be 1000 less the current value of myTotal
+        // print out the value of mylastone
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+
+
+
+
+
+    }
+}
